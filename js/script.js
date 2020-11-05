c32d07f41815a2d23760713c550a17d87311775a
viewtifult <tashanduncan1994@gmail.com>
2020-11-05 19:58:15 +0000

    Used Function for Color

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 let html = '';
 
-let color = Math.floor(Math.random() * 256);
+const color = () => Math.floor(Math.random() * 256);
 
 
 for (let i = 1; i <= 10; i++) {
-    red = Math.floor(Math.random() * 256);
-    green = Math.floor(Math.random() * 256);
-    blue = Math.floor(Math.random() * 256);
-    let randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+    let randomRGB = `rgb( ${color()}, ${color()}, ${color()} )`;
     html += `<div style="background-color: ${randomRGB}">${i}</div>`;
 }
 
